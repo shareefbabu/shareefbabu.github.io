6744dd1607b3a9bd56ff5dffe0a84e8ee1eb5221
Shareef <shareefbabu1@gmail.com>
2026-08-07 22:49:02 +0530

    Update paper acceptance entries in updates.js

--- updates.js
+++ updates.js
@@ -5,8 +5,12 @@
 
 const updates = [
     'Organiser – <a href="https://displace2026.github.io/" target="_blank">DISPLACE-M Challenge 2026</a>',
-    'Paper accepted - <b> Interspeech 2026</b>, Sydney, Australia. ',
-    'Paper accpeted -  <b>Fusion-based Deep Learning Approach for Estrus Detection for Low-Resource Animal audio Using Augmentation Techniques, ISED-2026</b>, Warangal, India.',
+    
+    'Paper accepted - <b><em>Benchmarking Speech Systems for Frontline Health Conversations: The DISPLACE-M Challenge</em>, Interspeech 2026</b>, Sydney, Australia. ',
+
+    'Paper accepted - <b><em>Sensor-Enabled Automated Bioacoustic Systems for Precision Livestock Management and Wildlife Welfare Monitor-ing: A Comprehensive Review, </em> International Journal on Smart Sensing and Intelligent Systems. </b>',
+    
+    'Paper accpeted -  <b>Fusion-based Deep Learning Approach for Estrus Detection for Low-Resource Animal audio Using Augmentation Techniques, 14th ISED-2026</b>, NIT Warangal, India.',
 
     'Paper accepted – <b>CE2CT 2026</b>, Bhimtal, Nainital, India.',
 
