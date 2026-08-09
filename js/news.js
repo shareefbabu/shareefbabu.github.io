d258b095abaf7262d28507221e3d852df8fb5f57
Shareef <shareefbabu1@gmail.com>
2026-08-09 18:45:47 +0530

    Update news items with recent achievements

--- js/news.js
+++ js/news.js
@@ -1,51 +1,82 @@
 const newsItems = [
     {
-        date: "December, 2024",
-        text: "Joined as Assistant Professor in the CSE Dept. at IIT Jammu."
+        date: "August , 2026",
+        text: "One Paper accepted at 14th ISED, NIT Warangal India."
+    },
+    
+    {
+        date: "July, 2026",
+        text: "One Paper accepted at International Journal on Smart Sensing and Intelligent Systems."
+    },
+    
+    {
+        date: "June, 2026",
+        text: "One Paper accepted at Interspeech 2026, Sydney, Australia ."
+    },
+    
+    {
+        date: "June, 2026",
+        text: "Joined as Assistant Professor in the School of Electrical Engineering at Manipal Institute of Technology Bangalore, MAHE."
+    },
+    
+    {
+        date: "April, 2026",
+        text: "One paper accepted at CE2CT 2026,Bhimtal, Nainital, Uttarakhand, India."
     },
 
     {
-        date: "October, 2024",
-        text: "One paper accepted at European Journal of Transport and Infrastructure Research."
+        date: "Jan, 2026",
+        text: "Organised one week FDP on Emerging Frontiers in Speech and Language Technologies."
+    },
+
+    {
+        date: "December, 2025",
+        text: "Session Chair for 17th IEEE International Conference on Computational Intelligence and Communication Networks (CICN 2025), NIT Goa."
+    },
+
+    {
+        date: "October, 2025",
+        text: "Organised Challenge - DISPLACE 2026 CHALLENGE, DIarization and Speech Processing for LAnguage understanding in Conversational Environments (DISPLACE-M)."
+    },
+    
+    {
+        date: "August, 2025",
+        text: "One paper accepted at 38th IEEE Region 10 Conference 2025 TENCON 2025, Kota Kinabalu, Malaysia."
+    },
+
+    {
+        date: "August, 2025",
+        text: "One paper accepted at The 17th Asia Pacific Signal and Information Processing Association Annual Summit and Conference, October 22nd–24th 2025, Shangri-la, Singapore."
+    },
+
+    {
+        date: "December, 2024",
+        text: " Joined as Assistant professor - senior scale in the School of Computer Science at UPES Dehradun."
     },
 
     {
         date: "June, 2024",
-        text: "Joined LEAP Lab as a Postdoctoral researcher."
-    },
-
-    {
-        date: "May, 2024",
-        text: "One paper accepted at ACM Transactions on Multimedia Computing, Communications, and Applications."
+        text: "One paper accepted at Interspeech 2024, Kos Island, Greece."
     },
 
     {
         date: "December, 2023",
-        text: "One paper accepted at 49th IEEE International Conference on Acoustics, Speech, & Signal Processing (ICASSP) to be held at Seoul, Korea during 14–19 April 2024."
+        text: "Organised Challenge - Second DISPLACE 2024 CHALLENGE."
     },
 
     {
         date: "November, 2023",
-        text: "Attended and presented paper at 13th SESAR Innovations Days held at University of Seville, Spain during 27–30 November 2023."
+        text: "Joined LEAP Lab, IISc as a Postdoc."
     },
 
+    
     {
-        date: "November, 2023",
-        text: "One paper accepted at the 13th SESAR Innovations Days to be held at Seville, Spain in November 2023."
+        date: "June, 2021",
+        text: "Successfully defended my Ph.D. thesis at NIT Surathkal and completed my doctorate."
     },
-
+    
     {
-        date: "October, 2023",
-        text: "Two papers accepted at the 25th International Conference on Speech and Computer to be held at Hubli-Dharwad, India in November 2023."
+        date: "May, 2021",
+        text: "Joined IUI Lab, Seikei University, Tokyo, Japan  as a Postdoc."
     },
-
-    {
-        date: "May, 2023",
-        text: "Joined IDIAP Research Institute in Switzerland as a Postdoc."
-    },
-
-    {
-        date: "January, 2023",
-        text: "Successfully defended my Ph.D. thesis at IIT Guwahati and completed my doctorate."
-    }
 ];
